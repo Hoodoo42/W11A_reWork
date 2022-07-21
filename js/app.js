ffb98606ca26982e961e5830325e760f566819e6
Hoodoo42 <create@hoodooverse.ca>
2022-07-21 11:43:05 -0600

    This commit is a point of where I am getting stuck and every option, even though I see it in the debugger holding the correct information, as soon as it get to the if() it switches out what the chosenPlanet is and I need to figure out why

--- js/app.js
+++ js/app.js
@@ -1,41 +1,47 @@
-// original working code from first part of assignment.
-
-// function jupiterSelection(details){
-//     Cookies.set(`selection`, `jupiter`);
-//     location.href = `selection.html`
-// };
-
-// function saturnSelection(details){
-//     Cookies.set(`selection`, `saturn`);
-//     location.href = `selection.html`
-// };
-
-// function plutoSelection(details){
-//     Cookies.set(`selection`, `pluto`);
-//     location.href = `selection.html`
-// };
-
-// let jupiter = document.getElementById(`planetOne`);
-// jupiter.addEventListener(`click`, jupiterSelection)
-
-// let saturn = document.getElementById(`planetTwo`);
-// saturn.addEventListener(`click`, saturnSelection)
-
-// let pluto = document.getElementById(`planetThree`);
-// pluto.addEventListener(`click`, plutoSelection)
-
 function jupiterSelection(details) {
-  Cookies.set(`selection`, `jupiter`);
+  let chooseJupiter = {
+    name: `Jupiter`,
+    radius: `69,911 km`,
+    ranking: 5,
+    isBiggerThanEarth: true,
+  };
+
+  // I need a variable to store the stringd object
+  // let jupiterJson is the variable storing the stringd object.
+  // JSON.stringify is the function turning the chooseJupiter object into a string.
+  let jupiterJson = JSON.stringify(chooseJupiter);
+  // I now need to save the newly stringd object into a cookie
+  // I named the cookie selection to keep the same cookie.get flow
+  // the cookie value is the variable holding the stringd version of the object.
+  Cookies.set(`selection`, jupiterJson);
+  // this line will take user to the selection page
   location.href = `selection.html`;
 }
 
 function saturnSelection(details) {
-  Cookies.set(`selection`, `saturn`);
+  let chooseSaturn = {
+    name: `Saturn`,
+    radius: `58,232 km`,
+    ranking: 6,
+    isBiggerThanEarth: true,
+  };
+
+  let saturnJson = JSON.stringify(chooseSaturn);
+
+  Cookies.set(`selection`, saturnJson);
   location.href = `selection.html`;
 }
 
 function plutoSelection(details) {
-  Cookies.set(`selection`, `pluto`);
+  let choosePluto = {
+    name: `Pluto`,
+    radius: `1,188.3 km`,
+    ranking: 9,
+    isBiggerThanEarth: false,
+  };
+
+  let plutoJson = JSON.stringify(choosePluto);
+  Cookies.set(`selection`, plutoJson);
   location.href = `selection.html`;
 }
 
@@ -47,3 +53,31 @@ saturn.addEventListener(`click`, saturnSelection);
 
 let pluto = document.getElementById(`planetThree`);
 pluto.addEventListener(`click`, plutoSelection);
+
+// Orignal working code for the first section of W11A
+
+// This function is saving the string `jupiter` which holds the button and its eventLister -into a cookie
+
+// function jupiterSelection(details) {
+//     Cookies.set(`selection`, `jupiter`);
+//     location.href = `selection.html`;
+//   }
+
+//   function saturnSelection(details) {
+//     Cookies.set(`selection`, `saturn`);
+//     location.href = `selection.html`;
+//   }
+
+//   function plutoSelection(details) {
+//     Cookies.set(`selection`, `pluto`);
+//     location.href = `selection.html`;
+//   }
+
+//   let jupiter = document.getElementById(`planetOne`);
+//   jupiter.addEventListener(`click`, jupiterSelection);
+
+//   let saturn = document.getElementById(`planetTwo`);
+//   saturn.addEventListener(`click`, saturnSelection);
+
+//   let pluto = document.getElementById(`planetThree`);
+//   pluto.addEventListener(`click`, plutoSelection);
--- js/selection.js
+++ js/selection.js
@@ -1,30 +1,39 @@
-// original working code for first part of assignemt
+let grabJson = Cookies.get(`selection`);
+let convertJson = JSON.parse(grabJson);
+
+let chosenPlanet = convertJson;
+
+let setMessage = document.getElementById(`message`);
+
+if (chosenPlanet === `chooseJupiter`) {
+  setMessage[`innerHTML`] = `<h3>You chose Jupiter! Jupiter Has 67 Moons!`;
+} else if ((chosenPlanet = `chooseSaturn`)) {
+  setMessage[
+    `innerHTML`
+  ] = `<h3>You chose Saturn! A year on Saturn is more than 29 Earth years!`;
+} else if ((chosenPlanet = `choosePluto`)) {
+  setMessage[
+    `innerHTML`
+  ] = `<h3>You chose Pluto! Pluto has more water than earth!`;
+} else {
+  setMessage[
+    `innerHTML`
+  ] = `<h3>You chose have not chosen a planet, please go select one.`;
+}
+
+// Origianl working code for the first section of W11A
 
 // let chosenPlanet = Cookies.get(`selection`);
 // let setMessage = document.getElementById(`message`);
 
-// if(chosenPlanet === `jupiter`){
-//     setMessage[`innerHTML`] = `<h3>You chose Jupiter! Jupiter Has 67 Moons!`;
-
-// }else if(chosenPlanet === `saturn`){
-//     setMessage[`innerHTML`] = `<h3>You chose Saturn! A year on Saturn is more than 29 Earth years!`;
-
-// }else if(chosenPlanet === `pluto`){
-//     setMessage[`innerHTML`] = `<h3>You chose Pluto! Pluto has more water than earth!`;
-
+// if (chosenPlanet === `jupiter`) {
+//   setMessage[`innerHTML`] = `<h3>You chose Jupiter! Jupiter Has 67 Moons!`;
+// } else if (chosenPlanet === `saturn`) {
+//   setMessage[
+//     `innerHTML`
+//   ] = `<h3>You chose Saturn! A year on Saturn is more than 29 Earth years!`;
+// } else if (chosenPlanet === `pluto`) {
+//   setMessage[
+//     `innerHTML`
+//   ] = `<h3>You chose Pluto! Pluto has more water than earth!`;
 // }
-
-let chosenPlanet = Cookies.get(`selection`);
-let setMessage = document.getElementById(`message`);
-
-if (chosenPlanet === `jupiter`) {
-  setMessage[`innerHTML`] = `<h3>You chose Jupiter! Jupiter Has 67 Moons!`;
-} else if (chosenPlanet === `saturn`) {
-  setMessage[
-    `innerHTML`
-  ] = `<h3>You chose Saturn! A year on Saturn is more than 29 Earth years!`;
-} else if (chosenPlanet === `pluto`) {
-  setMessage[
-    `innerHTML`
-  ] = `<h3>You chose Pluto! Pluto has more water than earth!`;
-}
